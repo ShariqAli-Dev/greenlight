fc8927f801f54ab1a10ee521b2d171fd16672c06
shariqali-dev <shariqali.dev@gmail.com>
2025-05-03 20:49:35 -0400

    7.2 creating a new movie

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"time"
 
+	"github.com/lib/pq"
 	"github.com/shariqali-dev/greenlight/internal/validator"
 )
 
@@ -38,7 +39,15 @@ type MovieModal struct {
 	DB *sql.DB
 }
 
-func (m MovieModal) Insert(movie *Movie) error { return nil }
+func (m MovieModal) Insert(movie *Movie) error {
+	query := `
+        INSERT INTO movies (title, year, runtime, genres) 
+        VALUES ($1, $2, $3, $4)
+        RETURNING id, created_at, version`
+
+	args := []any{movie.Title, movie.Year, movie.Runtime, pq.Array(movie.Genres)}
+	return m.DB.QueryRow(query, args...).Scan(&movie.ID, &movie.CreatedAt, &movie.Version)
+}
 
 func (m MovieModal) Get(id int64) (*Movie, error) {
 	return nil, nil
